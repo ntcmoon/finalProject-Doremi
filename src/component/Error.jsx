981c2aa6e5f2609bce757478c21c5de366a9cd9a
wdeachac <wannapornj.dea@gmail.com>
2024-09-08 02:23:59 +0700

    error css decoration

--- src/component/Error.jsx
+++ src/component/Error.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect } from "react";
 
 export default function Error() {
-
   return (
     <div>
-      <h1>Opps, I did it again!</h1>
-      <p>something goes wrong, please hit F5. JKD</p>
+      <h1 className="text-3xl text-amber-500 font-bold">
+        Opps, I did it again!
+      </h1>
+      <h3 className="text-xl text-amber-700 font-bold underline decoration-wavy underline-offset-1">
+        404 NOT FOUND
+      </h3>
     </div>
   );
 }
